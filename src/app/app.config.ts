3d57fff519a85bee8caa9a9cf3c5778c78bd3bf0
Jan Mrak <janmrak1@gmail.com>
2024-07-10 00:22:50 +0200

    feat: chart.js

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,7 +4,8 @@ import { provideAnimations } from '@angular/platform-browser/animations';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
 
 export const appConfig: ApplicationConfig = {
-    providers: [provideRouter(routes), provideAnimations(), provideAnimationsAsync()],
+    providers: [provideRouter(routes), provideAnimations(), provideAnimationsAsync(), provideCharts(withDefaultRegisterables())],
 };
