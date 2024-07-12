f11ce1b125d8e63e920ad5e8a01bb56f8ca22c55
Jan Mrak <janmrak1@gmail.com>
2024-07-13 00:47:47 +0200

    refactor: Messages component wrapper

--- src/app/features/statistics/statistics-routing.module.ts
+++ src/app/features/statistics/statistics-routing.module.ts
@@ -4,11 +4,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { OverviewComponent } from './components/overview/overview.component';
 import { UsersComponent } from './components/users/users.component';
 import { CommandsComponent } from './components/commands/commands.component';
+import { MessagesComponent } from './components/messages/messages.component';
 
 export const statisticsRoutes: Routes = [
     { path: 'overview', component: OverviewComponent },
     { path: 'users', component: UsersComponent },
     { path: 'commands', component: CommandsComponent },
+    { path: 'messages', component: MessagesComponent },
     { path: '', redirectTo: 'overview', pathMatch: 'full' },
 ];
 
--- src/app/features/statistics/statistics.module.ts
+++ src/app/features/statistics/statistics.module.ts
@@ -2,18 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { BaseChartDirective } from 'ng2-charts';
+import 'chartjs-adapter-date-fns';
 
 // Import components
 import { OverviewComponent } from './components/overview/overview.component';
 import { UsersComponent } from './components/users/users.component';
+import { MessagesPerDayComponent } from './components/messages/messages-per-day/messages-per-day.component';
 import { CommandsComponent } from './components/commands/commands.component';
 
 // Import services
-import { GraphDataService } from './services/graph-data.service';
 
 // Import feature routing
 import { statisticsRoutes } from './statistics-routing.module';
 import { HttpClientModule } from '@angular/common/http';
+import { MessagesPerChannelComponent } from './components/messages/messages-per-channel/messages-per-channel.component';
 
 @NgModule({
     declarations: [OverviewComponent, UsersComponent, CommandsComponent],
