8c90fd7ad69d7a951f47b6a7ec2416df69bb9ea0
Jan Mrak <janmrak1@gmail.com>
2024-07-15 00:10:38 +0200

    refactor: prettier
    Signed-off-by: Jan Mrak <janmrak1@gmail.com>

--- src/app/features/statistics/components/users/users.component.spec.ts
+++ src/app/features/statistics/components/users/users.component.spec.ts
@@ -3,21 +3,20 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { UsersComponent } from './users.component';
 
 describe('UsersComponent', () => {
-  let component: UsersComponent;
-  let fixture: ComponentFixture<UsersComponent>;
+    let component: UsersComponent;
+    let fixture: ComponentFixture<UsersComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [UsersComponent]
-    })
-    .compileComponents();
-    
-    fixture = TestBed.createComponent(UsersComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [UsersComponent],
+        }).compileComponents();
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+        fixture = TestBed.createComponent(UsersComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
