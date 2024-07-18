811e71e3ff02812db36da9fd14d81081e409e59c
Jan Mrak <janmrak1@gmail.com>
2024-07-18 23:57:55 +0200

    refactor: changed users to music statistics
    Signed-off-by: Jan Mrak <janmrak1@gmail.com>

--- src/app/features/statistics/statistics-routing.module.ts
+++ src/app/features/statistics/statistics-routing.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { OverviewComponent } from './components/overview/overview.component';
-import { UsersComponent } from './components/users/users.component';
+import { MusicComponent } from './components/music/music.component';
 import { MessagesComponent } from './components/messages/messages.component';
 import { VoiceComponent } from './components/voice/voice.component';
 
 export const statisticsRoutes: Routes = [
     { path: 'overview', component: OverviewComponent },
-    { path: 'users', component: UsersComponent },
+    { path: 'music', component: MusicComponent },
     { path: 'voice', component: VoiceComponent },
     { path: 'messages', component: MessagesComponent },
     { path: '', redirectTo: 'overview', pathMatch: 'full' },
