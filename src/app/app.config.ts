14b125231d4f233e5a08c92f77b715b5e0457f66
Jan Mrak <janmrak1@gmail.com>
2024-07-21 22:38:36 +0200

    refactor: moved sidebar&navbar to components subfolder
    Signed-off-by: Jan Mrak <janmrak1@gmail.com>

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,7 +6,18 @@ import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
 import { provideHttpClient } from '@angular/common/http';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
+import { guildReducer } from './core/store/reducers/guild.reducer';
 
 export const appConfig: ApplicationConfig = {
-    providers: [provideRouter(routes), provideHttpClient(), provideAnimations(), provideAnimationsAsync(), provideCharts(withDefaultRegisterables())],
+    providers: [
+        provideRouter(routes),
+        provideHttpClient(),
+        provideAnimations(),
+        provideAnimationsAsync(),
+        provideCharts(withDefaultRegisterables()),
+        provideStore({ guild: guildReducer }),
+        provideEffects(),
+    ],
 };
