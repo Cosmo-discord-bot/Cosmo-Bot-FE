37aa360524fc2285613451b51db986ca7f06450d
Jan Mrak <janmrak1@gmail.com>
2024-08-10 01:41:12 +0200

    feat: prepped for info about guild

---
+++ src/app/features/statistics/components/guild/guild.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-guild',
+  standalone: true,
+  imports: [],
+  templateUrl: './guild.component.html',
+  styleUrl: './guild.component.scss'
+})
+export class GuildComponent {
+
+}
