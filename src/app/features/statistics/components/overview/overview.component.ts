bb7362fac52c7b9c9a06b1067d025aef966cb400
Jan Mrak <janmrak1@gmail.com>
2024-08-10 01:40:50 +0200

    refactor: remove unused components

--- src/app/features/statistics/components/overview/overview.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    standalone: true,
-    selector: 'app-statistics-overview',
-    templateUrl: './overview.component.html',
-    styleUrls: ['./overview.component.scss'],
-})
-export class OverviewComponent {}
